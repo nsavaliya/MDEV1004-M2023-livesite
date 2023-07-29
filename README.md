9b7bf107dc023d914ba0e7a349276f80999222fe
Namrata Savaliya <savaliyanamrata2002@gmail.com>
2023-07-29 18:39:08 -0400

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# MDEV1004 - M2023 - ICE 5
-Demo project for In-Class Exercise 5 in the Semester at Georgian College
--- Server/Config/db.ts
+++
@@ -1,9 +0,0 @@
-let localURI= 'mongodb://127.0.0.1:27017/movies';
-let remoteURI='mongodb+srv://nsavaliya:hBiHE2GKc9ifWBBf@moviecluster.bpwbiyk.mongodb.net/movie';
-let secret = 'SomeSecret';
-
-export default {
-    localURI: localURI,
-    remoteURI: remoteURI,
-    secret: secret
-}
