c4f7d49b1e0860e62967098cffef92465ae83ea3
Namrata Savaliya <savaliyanamrata2002@gmail.com>
2023-07-29 18:44:56 -0400

    added all files from ICE9 to livesite

---
+++ Server/Config/db.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let localURI = 'mongodb://127.0.0.1:27017/movies';
+let remoteURI = 'mongodb+srv://nsavaliya:hBiHE2GKc9ifWBBf@moviecluster.bpwbiyk.mongodb.net/movies';
+let secret = 'SomeSecret';
+exports.default = {
+    localURI: localURI,
+    remoteURI: remoteURI,
+    secret: secret
+};
+//# sourceMappingURL=db.js.map
---
+++ Server/Config/db.ts
@@ -0,0 +1,8 @@
+let localURI= 'mongodb://127.0.0.1:27017/movies';
+let remoteURI='mongodb+srv://nsavaliya:hBiHE2GKc9ifWBBf@moviecluster.bpwbiyk.mongodb.net/movies';
+let secret  = 'SomeSecret';
+export default {
+    localURI: localURI,
+    remoteURI: remoteURI,
+    secret: secret
+}
